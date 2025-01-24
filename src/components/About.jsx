81772f11a13ac0f891bf907398ca17e9d88c9397
HarshaSiriki <87467222+HarshaSiriki@users.noreply.github.com>
2025-01-23 22:33:31 -0500

    updated profile summary

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -53,10 +53,7 @@ const About = () => {
         variants={fadeIn("","",0.1,1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I am a Software Engineer with experience in React, C#, JavaScript, HTML5 & CSS, Python, MySQL, and
-        proficient in tools like JIRA, Zendesk, Wireshark, postman, SSMS, Figma, VS Code, Visual Studio.  
-        I have some industry expertise in unit testing, API support, quality assurance, TCP, VoIP, and networking protocols. 
-        I'm a quick learner and collaborate closely with clients to solve their problems. I am a web3 enthusiast and learning skills to become a web3.0 developer.
+        A highly skilled Software Engineer with over 3 years of experience in frontend development, API integration, and technical support. Profi cient in Python, React.js, with hands-on experience in cloud-based technologies, API testing, and performance optimization. Demonstrated expertise in troubleshooting complex issues, including VoIP protocols and Cisco CUCM, and building scalable solutions. Adept at collaborating in Agile environments, and experienced with CI/CD pipelines and cloud services. A proactive self-starter who leads projects, improves processes, and drives results while maintaining a strong focus on team collaboration and problem-solving.
       </motion.p>
 
       <div
